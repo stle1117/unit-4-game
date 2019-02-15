a953f5568c1ecff8646c6f9c0a78516c2d45a6f4
Astrid <leigh.stevens.ls@gmail.com>
2019-02-14 18:28:00 -0600

    game working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,76 +2,78 @@ $(document).ready(function() {
 
 
     //declare global variables
-    
-        var firstCrystalNumber = "";
-        var secondCrystalNumber = "";
-        var thirdCrystalNumber = "";
-        var fourthCrystalNumber = "";
-        var randomTotal = "";
-        var newrandomTotal = "";
-        var totalScoreCounter = 0;
-        var winsCounter = "";
-        var lossesCounter = "";
+
+    var firstCrystalNumber = "";
+    var secondCrystalNumber = "";
+    var thirdCrystalNumber = "";
+    var fourthCrystalNumber = "";
+    var randomTotal = 0;
+    var newrandomTotal = 0;
+    var totalScoreCounter = 0;
+    var winsCounter = 0;
+    var lossesCounter = 0;
     
     
     //create a function to initialize the game once the page loads.  
-        function initializeGame () {
-    
-            firstCrystalNumber = Math.floor(Math.random() * 10) +1;
-    
-            secondCrystalNumber = Math.floor(Math.random() * 10) +1;
-    
-            thirdCrystalNumber = Math.floor(Math.random() * 10) +1;
-    
-            fourthCrystalNumber = Math.floor(Math.random() * 10) +1;
-    
-            randomTotal = Math.floor(Math.random() * 100) +1;
-            $("#random-number").text(randomTotal);
-    
-        }
+    function initializeGame () {
+
+        firstCrystalNumber = Math.floor(Math.random() * 10) +1;
+
+        secondCrystalNumber = Math.floor(Math.random() * 10) +1;
+
+        thirdCrystalNumber = Math.floor(Math.random() * 10) +1;
+
+        fourthCrystalNumber = Math.floor(Math.random() * 10) +1;
+
+        randomTotal = Math.floor(Math.random() * 100) +1;
+        $("#random-number").text(randomTotal);
+
+    }
     
     /*create on-click events that capture the value of the user's selection
     and performs various operations on it */
     
-        function playGame () {
+    function playGame () {
     
-    $("#img1").on("click", function() {
-        totalScoreCounter = totalScoreCounter + firstCrystalNumber
-        $("#total-score").text(totalScoreCounter);
-    })
+        $("#img1").on("click", function() {
+            totalScoreCounter = totalScoreCounter + firstCrystalNumber
+            $("#total-score").text(totalScoreCounter);
+            checkTotal();
+        })
+        
+        
+        $("#img2").on("click", function() {
+            totalScoreCounter = totalScoreCounter + secondCrystalNumber
+            $("#total-score").text(totalScoreCounter);
+            checkTotal();
+        })
+        
+        
+        $("#img3").on("click", function() {
+            totalScoreCounter = totalScoreCounter + thirdCrystalNumber
+            $("#total-score").text(totalScoreCounter);
+            checkTotal();
+        })
+        
+        $("#img4").on("click", function() {
+            totalScoreCounter = totalScoreCounter + fourthCrystalNumber
+            $("#total-score").text(totalScoreCounter);
+            checkTotal();
+        })
     
-    
-    $("#img2").on("click", function() {
-        totalScoreCounter = totalScoreCounter + secondCrystalNumber
-        $("#total-score").text(totalScoreCounter);
-    })
-    
-    
-    $("#img3").on("click", function() {
-        totalScoreCounter = totalScoreCounter + thirdCrystalNumber
-        $("#total-score").text(totalScoreCounter);
-    })
-    
-    $("#img4").on("click", function() {
-        totalScoreCounter = totalScoreCounter + fourthCrystalNumber
-        $("#total-score").text(totalScoreCounter);
-    })
-    
+    }
     
     //set-up while statement: while totalScore < randomTotal, continue playing
     
+    while (totalScoreCounter<randomTotal); {
+        playGame()
     }
-        while (totalScoreCounter<randomTotal); {
-            playGame()
-        }
     
     /*set-up if...else if statement: if totalScore = randomTotal...wins, reset
     else if totalScore > randomTotal...losses, reset*/
     
     //write reset function
-        function resetGame ()
-                
-    {
+    function resetGame () {
     
         firstCrystalNumber = Math.floor(Math.random() * 10) +1;
     
@@ -84,24 +86,25 @@ $(document).ready(function() {
         newrandomTotal = Math.floor(Math.random() * 100) +1;
             $("#random-number").text(newrandomTotal);
     
-        TotalScore = 0
+        totalScoreCounter = 0
     }
        
-        if (totalScoreCounter === randomTotal); {
+    function checkTotal () {
+        if (totalScoreCounter === randomTotal) {
             winsCounter++;
-            $("#wins").text(winsCounter);
+            $("#wins").text("Wins: " + winsCounter);
             resetGame();
         }
-    
-        if (totalScoreCounter > randomTotal); {
+
+        if (totalScoreCounter > randomTotal) {
             lossesCounter++;
-            $("#losses").text(lossesCounter);
+            $("#losses").text("Losses: " + lossesCounter);
             resetGame()
         }
+
+    }
     
     //call function to initialize the game
+    initializeGame();
     
-        initializeGame();
-    
-    
-    });
+});
